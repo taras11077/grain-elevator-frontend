e205b18bd4b8f4f089511d7968c69a856f258e0b
stg11077 <stg11077@gmail.com>
2024-12-16 00:19:42 +0200

    implemented use Bearer-token

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
 import { message } from 'antd'
-import { logout } from '../asyncThunks/authThunk'
+import axios from 'axios'
 import { store } from '../app/store'
+import { logout } from '../asyncThunks/authThunk'
 
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
@@ -9,6 +9,17 @@ const api = axios.create({
     headers: {
         'Content-Type': 'application/json'
     }
+});
+
+// Додавання токена перед кожним запитом
+api.interceptors.request.use(config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+}, error => {
+    return Promise.reject(error);
 });
 
 // Інтерцептор для обробки відповіді
