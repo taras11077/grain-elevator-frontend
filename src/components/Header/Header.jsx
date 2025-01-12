8be6dabf5934db4f16cdf83a9d39c9f50c3415ed
stg11077 <stg11077@gmail.com>
2025-01-12 20:27:13 +0200

    add styles & photo in HomePage

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,79 +1,110 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 import { logout } from '../../asyncThunks/authThunk'
 import './Header.css'
 
 const Header = () => {
 	const {token, userData} = useSelector((state) => state.auth);
 	const dispatch = useDispatch();
+	const location = useLocation(); // Отримуємо поточний шлях
+	const isHomePage = location.pathname === '/home'; // Перевіряємо, чи це сторінка Home
 
 	const logoutHandler = () => {
 		dispatch(logout());
 	}
 
 	return (
-		<div className='nav-container'>
-			<NavLink className='nav-link' to="/home">Home</NavLink>
+		<div className={`nav-container ${isHomePage ? 'home-page' : ''}`}>
+			{/* <NavLink className='nav-link' to="/home">Home</NavLink> */}
 
 			{(userData.role === 'Admin' || userData.role === 'CEO' || userData.role === 'Laboratory') && (
-        		<NavLink className="nav-link" to="/input-invoices">Прибуткові накладні</NavLink>
+        		<NavLink
+				className={`nav-link ${isHomePage ? 'home-page-link' : ''}`}
+				 to="/input-invoices">Прибуткові накладні</NavLink>
       		)}
 
 			{(userData.role === 'Admin' || userData.role === 'CEO' || userData.role === 'Laboratory') && (
-        		<NavLink className="nav-link" to="/laboratory-cards">Лабораторні картки</NavLink>
+        		<NavLink 
+				className={`nav-link ${isHomePage ? 'home-page-link' : ''}`}
+				to="/laboratory-cards">Лабораторні картки</NavLink>
       		)}
 
 			{(userData.role === 'Admin' || userData.role === 'CEO' || userData.role === 'Technologist') && (
-        		<NavLink className="nav-link" to="/register">Реєстри</NavLink>
+        		<NavLink 
+				className={`nav-link ${isHomePage ? 'home-page-link' : ''}`}
+				to="/register">Реєстри</NavLink>
       		)}
 
 			{(userData.role === 'Admin' || 
 					userData.role === 'CEO' || 
 					userData.role === 'Technologist' || 
 					userData.role === 'Accountant') && (
-        		<NavLink className="nav-link" to="/technological-operation">Технологичні операції</NavLink>
+        		<NavLink 
+				className={`nav-link ${isHomePage ? 'home-page-link' : ''}`}
+				to="/technological-operation">Технологичні операції</NavLink>
       		)}
 
 			{(userData.role === 'Admin' || 
 					userData.role === 'CEO' || 
 					userData.role === 'Technologist' || 
 					userData.role === 'Accountant') && (
-        		<NavLink className="nav-link" to="/completion-report">Акти виконаних робіт</NavLink>
+        		<NavLink 
+				className={`nav-link ${isHomePage ? 'home-page-link' : ''}`}
+				to="/completion-report">Акти виконаних робіт</NavLink>
       		)}
 
 			{(userData.role === 'Admin' || userData.role === 'CEO' || userData.role === 'Accountant') && (
-        		<NavLink className="nav-link" to="/price-list">Прайси</NavLink>
+        		<NavLink 
+				className={`nav-link ${isHomePage ? 'home-page-link' : ''}`}
+				to="/price-list">Прайси</NavLink>
       		)}
 
 			{(userData.role === 'Admin' || userData.role === 'CEO' || userData.role === 'Accountant') && (
-        		<NavLink className="nav-link" to="/warehouse">Склад</NavLink>
+        		<NavLink 
+				className={`nav-link ${isHomePage ? 'home-page-link' : ''}`}
+				to="/warehouse">Склад</NavLink>
       		)}
 
 			{(userData.role === 'Admin' || userData.role === 'CEO' || userData.role === 'Accountant') && (
-        		<NavLink className="nav-link" to="/output-invoices">Видаткові накладні</NavLink>
+        		<NavLink 
+				className={`nav-link ${isHomePage ? 'home-page-link' : ''}`} 
+				to="/output-invoices">Видаткові накладні</NavLink>
       		)}
 
 			{(userData.role === 'Admin' ||	userData.role === 'CEO'|| 	userData.role === 'Technologist' ||	userData.role === 'Laboratory' || userData.role === 'Accountant') && (
-        		<NavLink className="nav-link" to="/suppliers">Постачальники</NavLink>
+        		<NavLink 
+				className={`nav-link ${isHomePage ? 'home-page-link' : ''}`}
+				to="/suppliers">Постачальники</NavLink>
       		)}
 
 			{(userData.role === 'Admin' || 	userData.role === 'CEO'|| userData.role === 'Technologist' || userData.role === 'Laboratory' ||	userData.role === 'Accountant') && (
-        		<NavLink className="nav-link" to="/products">Продукція</NavLink>
+        		<NavLink 
+				className={`nav-link ${isHomePage ? 'home-page-link' : ''}`}
+				to="/products">Продукція</NavLink>
       		)}
 
 
 			{(userData.role === 'Admin' || userData.role === 'HR'|| userData.role === 'CEO') && (
-        		<NavLink id='nav-link' className='nav-link' to="/employee">Співробітники</NavLink>
+        		<NavLink id='nav-link'
+				className={`nav-link ${isHomePage ? 'home-page-link' : ''}`}
+				  to="/employee">Співробітники</NavLink>
       		)}
 
 			{(userData.role === 'Admin' || userData.role === 'HR'|| userData.role === 'CEO') && (
-        		<NavLink id='nav-link' className='nav-link' to="/role">Ролі</NavLink>
+        		<NavLink id='nav-link' 
+				className={`nav-link ${isHomePage ? 'home-page-link' : ''}`}
+				 to="/role">Ролі</NavLink>
       		)}
 
 			{(token) && (
 				<div>
-					{userData.name}
+					<div
+					className={`user-name ${isHomePage ? 'home-page-user-name' : ''}`}
+					>
+						{userData.name}
+					</div>
+					
 					<button onClick={logoutHandler}>Вихід</button>
 				</div>			
 			 )}
--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,65 +1,9 @@
-// import { Button, Form, Input, message } from 'antd'
-// import React, { useState } from 'react'
-// import { login } from '../../api/authApi'
-
-// const LoginForm = ({ onLoginSuccess }) => {
-//   const [loading, setLoading] = useState(false);
-
-//   const onFinish = async (values) => {
-//     try 
-// 	{
-// 			setLoading(true);
-// 			const token = await login(values.email, values.password);
-// 			localStorage.setItem('token', token); // збереження токена
-// 			message.success('Авторизація успішна!');
-// 			onLoginSuccess();
-			
-//     } catch (error) {
-//      		message.error(
-//         	error.response?.data?.message || 'Помилка під час авторизації!'
-//       );
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-
-//   return (
-//     <Form onFinish={onFinish} layout="vertical">
-//       <Form.Item
-//         label="Електронна пошта"
-//         name="email"
-//         rules={[
-//           { required: true, message: 'Будь ласка, введіть електронну пошту!' },
-//           { type: 'email', message: 'Введіть коректну електронну пошту!' },
-//         ]}
-//       >
-//         <Input />
-//       </Form.Item>
-
-//       <Form.Item
-//         label="Пароль"
-//         name="password"
-//         rules={[
-//           { required: true, message: 'Будь ласка, введіть пароль!' },
-//         ]}
-//       >
-//         <Input.Password />
-//       </Form.Item>
-
-//       <Button type="primary" htmlType="submit" loading={loading}>
-//         	Увійти
-//       </Button>
-//     </Form>
-//   );
-// };
-
-// export default LoginForm;
-
 import { Button, Form, Input, Modal } from 'antd'
 import React from "react"
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { login } from '../../asyncThunks/authThunk'
+import './Login.css';
 
 const LoginForm = ({closeModal}) => {
 
